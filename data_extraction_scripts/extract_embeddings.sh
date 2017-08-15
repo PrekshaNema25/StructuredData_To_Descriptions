758f6b90141b008faf80b38cb32e2385f1f6aaa5
PrekshaNema25 <preksha.nema9@gmail.com>
2017-08-15 17:23:50 +0000

    .

---
+++ data_extraction_scripts/extract_embeddings.sh
@@ -0,0 +1,27 @@
+# Given the language, extract the corresponding embedding in the given folder 
+
+#Usage:
+# sh extract_embedding <lang_name> <out_folder
+
+str=$1
+english="english"
+french="french"
+german="german"
+
+if [ "$str" == "$english" ]
+then
+	wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+	unzip glove.840B.300d.zip
+	mv glove.840B.300d.txt $2
+	sed -i '1 i\2196017 300' $2/glove.800B.300d.txt
+	mv $2/glove.800B.300d.txt $2/embeddings
+elif [ "$str" == "$french" ]
+then
+	wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.fr.vec
+	mv wiki.fr.vec $2/embeddings
+elif [ "$str" == "$german" ]
+then
+	wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.nl.vec
+	mv wiki.nl.vec $2/embeddings
+fi
+
