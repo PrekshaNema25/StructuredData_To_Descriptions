c967d0e200c17d3f25f0fc076a01b1a742b9779b
PrekshaNema25 <cs15d201@smail.iitm.ac.in>
2018-10-04 19:37:12 +0530

    Update extract_embeddings.sh

--- data_extraction_scripts/extract_embeddings.sh
+++ data_extraction_scripts/extract_embeddings.sh
@@ -14,8 +14,8 @@ then
 	wget http://nlp.stanford.edu/data/glove.840B.300d.zip
 	unzip glove.840B.300d.zip
 	mv glove.840B.300d.txt $2
-	sed -i '1 i\2196017 300' $2/glove.800B.300d.txt
-	mv $2/glove.800B.300d.txt $2/embeddings
+	sed -i '1 i\2196017 300' $2/glove.840B.300d.txt
+	mv $2/glove.840B.300d.txt $2/embeddings
 elif [ "$str" == "$french" ]
 then
 	wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.fr.vec
